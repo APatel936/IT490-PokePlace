ad7058734b85736eb346563eac2c9d9205163f41
APatel936 <54904659+APatel936@users.noreply.github.com>
2020-03-31 10:23:59 -0400

    Add files via upload

---
+++ Frontend/functions.php
@@ -0,0 +1,318 @@
+<?php
+require_once('../rabbitmqphp_example/path.inc');
+require_once('../rabbitmqphp_example/get_host_info.inc');
+require_once('../rabbitmqphp_example/rabbitMQLib.inc');
+
+session_start();
+$searchType = $_GET['searchType'];
+$pokeName = $_SESSION['pokeName'];
+$type = $_GET["type"];
+//  determines what kind of data was sent via javascript.js
+switch ($type) {
+    case "Login":
+        $username = $_GET["username"];
+        $password = $_GET["password"];
+        $response = login($username, $password);
+        echo $response;
+        break;
+
+    case "Logout":
+        session_destroy();
+        $response = true;
+        echo $response;
+        break;
+
+    case "Register":
+        $firstname = $_GET["firstname"];
+        $lastname = $_GET["lastname"];
+        $username = $_GET["username"];
+        $email = $_GET["email"];
+        $password = $_GET["password"];
+        $response = register($firstname, $lastname, $username, $email, $password);
+        echo $response;
+        break;
+
+    case "LoadCategories":
+        $request = array();
+
+        $request['type'] = "LoadCategories";
+        $response = createClientRequest($request);
+        echo $response;
+        break;
+
+    case "CreateCategory":
+        $request = array();
+
+        $request['type'] = "CreateCategories";
+        $request['cat_name'] = $_GET['catName'];
+        $request['cat_description'] = $_GET['catDesc'];
+
+        $response = createClientRequest($request);
+        echo $response;
+        break;
+
+    case "LoadTopics":
+        $request = array();
+
+        $request['type'] = "LoadTopics";
+        $request['cat_id'] = $_SESSION['cat_id'];
+        $response = createClientRequest($request);
+        echo $response;
+        break;
+
+    case "CreateTopic":
+        $request = array();
+
+        $request['type'] = "CreateTopic";
+        $request['topic_subject'] = $_GET['topicName'];
+        $request['cat_id'] = $_SESSION['cat_id'];
+        $request['username'] = $_SESSION['username'];
+
+        $response = createClientRequest($request);
+        echo $response;
+        break;
+
+    case "LoadPosts":
+        $request = array();
+
+        $request['type'] = "LoadPosts";
+        $request['topic_id'] = $_SESSION['topic_id'];
+        $response = createClientRequest($request);
+        echo $response;
+        break;
+
+    case "CreatePost":
+        $request = array();
+
+        $request['type'] = "CreatePosts";
+        $request['post_content'] = $_GET['postText'];
+        $request['topic_id'] = $_SESSION['topic_id'];
+        $request['username'] = $_SESSION['username'];
+
+        $response = createClientRequest($request);
+        echo $response;
+        break;
+
+    case "Search":
+        $request = array();
+        $request['type'] = "Search";
+
+        switch ($searchType){
+            case "name":
+                $request['name'] = $_GET['searchText'];
+                $request['pokemonNum'] = "na";
+                $request['pokeType'] = "na";
+                $request['ability'] = "na";
+                break;
+
+            case "pokemonNum":
+                $request['name'] = "na";
+                $request['pokemonNum'] = $_GET['searchText'];
+                $request['pokeType'] = "na";
+                $request['ability'] = "na";
+                break;
+
+            case "pokeType":
+                $request['name'] = "na";
+                $request['pokemonNum'] = "na";
+                $request['pokeType'] = $_GET['searchText'];
+                $request['ability'] = "na";
+                break;
+
+            case "ability":
+                $request['name'] = "na";
+                $request['pokemonNum'] = "na";
+                $request['pokeType'] = "na";
+                $request['ability'] = $_GET['searchText'];
+                break;
+            }
+
+        $response_json = createClientRequest($request);
+
+        $response = json_decode($response_json);
+        $tempString = "";
+
+        if ($response -> speed){
+
+            $_SESSION['pokeName'] = $response -> pokemonName;
+
+            $tempString .= '<h1 class="text-center">' .$response -> pokemonName . '</h1>';
+            $tempString .= '<table class="table table-hover table-dark">';
+            $tempString .= '<thead>';
+            $tempString .= '<tr>';
+                $tempString .= '<th><span class="tableTitle">HP</span></th>';
+                $tempString .= '<th><span class="tableTitle">Attack</span></th>';
+                $tempString .= '<th><span class="tableTitle">Defense</span></th>';
+                $tempString .= '<th><span class="tableTitle">Special Attack</span></th>';
+                $tempString .= '<th><span class="tableTitle">Special Defense</span></th>';
+                $tempString .= '<th><span class="tableTitle">Speed</span></th>';
+            $tempString .= '</tr>';
+            $tempString .= '</thead>';
+            $tempString .= '<tbody>';
+            $tempString .= '<tr class="text-center">';
+                $tempString .= '<td>' .$response -> hp . '</td>';
+                $tempString .= '<td>' .$response -> atk. '</td>';
+                $tempString .= '<td>' .$response -> def. '</td>';
+                $tempString .= '<td>' .$response -> spAtk. '</td>';
+                $tempString .= '<td>' .$response -> spDef. '</td>';
+                $tempString .= '<td>' .$response -> speed. '</td>';
+            $tempString .= '</tr>';
+            $tempString .= '</tbody>';
+        $tempString .= '</table>';
+            $tempString .= '<button type="button" id="addPokemon" class="btn btn-outline-warning btn-lg" onclick="addPokemon()" ><i class="fas fa-edit"></i> Add Pokemon to Team</button>';
+
+        }else {
+
+            $pokemonList = $response -> pokemonNames;
+
+            $tempString .= '<table class="table table-hover table-dark">';
+            $tempString .= "<thead>";
+            $tempString .= " <tr>";
+            $tempString .= '<th colspan="6"><span class="tableTitle">Search Results</span></th>';
+            $tempString .= "</tr>";
+            $tempString .= "</thead>";
+            $tempString .= "<tbody>";
+            $tempString .= "<tr>";
+            foreach ($pokemonList as $name) {
+                $tempString .= '<td><a href="pokemon.php?name=' . $name . '"><span class="categoryTitle">' . $name . '</span></a></td>';
+                $tempString .= "  </tr>";
+                $tempString .= " <tr>";
+            }
+            $tempString .= " </tbody>";
+            $tempString .= " </table>";
+        }
+        echo $tempString;
+
+        break;
+
+    case "SinglePokeSearch":
+        $request = array();
+
+        $request['type'] = "Search";
+        $request['name'] = $pokeName;
+        $request['pokemonNum'] = "na";
+        $request['pokeType'] = "na";
+        $request['ability'] = "na";
+
+        $response_json = createClientRequest($request);
+        $response = json_decode($response_json);
+
+        $tempString .= '<h1 class="text-center">' .$response -> pokemonName . '</h1>';
+        $tempString .= '<table class="table table-hover table-dark">';
+        $tempString .= '<thead>';
+        $tempString .= '<tr>';
+        $tempString .= '<th><span class="tableTitle">HP</span></th>';
+        $tempString .= '<th><span class="tableTitle">Attack</span></th>';
+        $tempString .= '<th><span class="tableTitle">Defense</span></th>';
+        $tempString .= '<th><span class="tableTitle">Special Attack</span></th>';
+        $tempString .= '<th><span class="tableTitle">Special Defense</span></th>';
+        $tempString .= '<th><span class="tableTitle">Speed</span></th>';
+        $tempString .= '</tr>';
+        $tempString .= '</thead>';
+        $tempString .= '<tbody>';
+        $tempString .= '<tr class="text-center">';
+        $tempString .= '<td>' .$response -> hp . '</td>';
+        $tempString .= '<td>' .$response -> atk. '</td>';
+        $tempString .= '<td>' .$response -> def. '</td>';
+        $tempString .= '<td>' .$response -> spAtk. '</td>';
+        $tempString .= '<td>' .$response -> spDef. '</td>';
+        $tempString .= '<td>' .$response -> speed. '</td>';
+        $tempString .= '</tr>';
+        $tempString .= '</tbody>';
+        $tempString .= '</table>';
+        $tempString .= '<button type="button" id="addPokemon" class="btn btn-outline-warning btn-lg" onclick="addPokemon()" ><i class="fas fa-edit"></i> Add Pokemon to Team</button>';
+
+
+        echo $tempString;
+        break;
+
+    case "LoadPokemon":
+        $request = array();
+
+        $request['type'] = "LoadPokemon";
+        $request['username'] = $_SESSION['username'];
+
+        $response = createClientRequest($request);
+        echo $response;
+        break;
+
+    case "AddPokemon":
+        $request = array();
+
+        $request['type'] = "AddPokemon";
+        $request['username'] = $_SESSION['username'];
+        $request['pokemonName'] = $_SESSION{'pokeName'};
+
+        $response = createClientRequest($request);
+        echo $response;
+        break;
+
+    case "LoadLeaderboard":
+        $request = array();
+
+        $request['type'] = "LoadLeaderboard";
+
+        $response = createClientRequest($request);
+        echo $response;
+        break;
+
+    case "Battle":
+
+        $request = array();
+
+        $request['type'] = "Battle";
+        $request['username_1'] = $_SESSION['username'];
+        $request['username_2'] = $_GET['useranme_toBattle'];
+
+        $response = createClientRequest($request);
+
+        echo $response;
+        break;
+
+}
+//  This function will send a login request message to Db through RabbitMQ
+function login($username, $password)
+{
+    $request = array();
+    $request['type'] = "Login";
+    $request['username'] = $username;
+    $request['password'] = $password;
+
+    $returnedValue = createClientRequest($request);
+
+    if ($returnedValue == true) {
+        $_SESSION["username"] = $username;
+        $_SESSION["loggedIn"] = true;
+    } else {
+        session_destroy();
+    }
+
+    return $returnedValue;
+}
+//  This function will send register request to RabbitMQ
+function register($firstname, $lastname, $username, $email, $password)
+{
+    $request = array();
+
+    $request['type'] = "Register";
+    $request['firstname'] = $firstname;
+    $request['lastname'] = $lastname;
+    $request['username'] = $username;
+    $request['password'] = $password;
+    $request['email'] = $email;
+
+    $returnedValue = createClientRequest($request);
+
+    return $returnedValue;
+}
+//  creates rabbitMq client request
+function createClientRequest($request){
+    $client = new rabbitMQClient("/home/bryan/git/IT490-Spring2020/rabbitmqphp_example/rabbitMQ_db.ini", "testServer");
+    $response = $client->send_request($request);
+
+    return $response;
+}
+
+?>
+
+
---
+++ Frontend/javascript.js
@@ -0,0 +1,352 @@
+function checkLoginCredentials(){
+    //  Taking Form input
+    let username = document.getElementById("username_login").value;
+    let password = document.getElementById("password_login").value;
+
+    if (username !== "" && password !== ""){
+        sendLoginCredentials(username, password);
+    }else{
+        alert("Please fill out all required information");
+    }
+}
+function sendLoginCredentials(username, password){
+    let httpReq = new XMLHttpRequest();
+    httpReq.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            document.getElementById("loginButtonId").innerHTML = "Login";
+
+            console.log(this.responseText);
+
+            if(this.responseText == 1){
+                alert("Logged in successfully");
+                window.location = "profile.php";
+            }else{
+                alert("Problem logging in.  Please try again");
+            }
+
+        }else{
+            document.getElementById("loginButtonId").innerHTML = "Loading...";
+        }
+    };
+    httpReq.open("GET", "functions.php?type=Login&username=" + username + "&password=" + password);
+    httpReq.send(null);
+}
+
+function logout() {
+    if (confirm('Are you sure you want log out?')) {
+        let httpReq = new XMLHttpRequest();
+        httpReq.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("logoutButtonId").innerHTML = "Logout";
+                console.log(this.responseText);
+
+                if (this.responseText == true) {
+                    alert("Logged out successfully");
+                    window.location = "index.php";
+                } else {
+                    alert("Problem logging out user.  Please try again");
+                }
+            } else {
+                document.getElementById("logoutButtonId").innerHTML = "Loading...";
+            }
+        };
+        httpReq.open("GET", "functions.php?type=Logout");
+        httpReq.send(null);
+    } else {
+        //cancels login request
+    }
+}
+//  Form validation for Register
+function checkRegisterCredentials(){
+    //  Taking Form input
+    let firstname = document.getElementById("id_firstname").value;
+    let lastname = document.getElementById("id_lastname").value;
+    let username = document.getElementById("id_username").value;
+    let email = document.getElementById("id_email").value;
+    let password = document.getElementById("id_password").value;
+    let confirmPassword = document.getElementById("id_confirm_password").value;
+
+    if (firstname !== "" && lastname !== "" && username !== "" && email !== "" && password !== "" && confirmPassword !== ""){
+       if (password == confirmPassword) {
+           sendRegisterCredentials(firstname, lastname, username, email, password);
+       }else{
+           alert("Password and Confirm Password must match");
+       }
+    }else{
+        alert("Please fill out all required information");
+    }
+}
+
+//  This function sends a AJAX request for Register new user
+function sendRegisterCredentials(firstname, lastname, username, email, password){
+    let httpReq = new XMLHttpRequest();
+    httpReq.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            document.getElementById("registerButtonId").innerHTML = "Register";
+            console.log(this.responseText);
+
+            if(this.responseText == true){
+                alert("Registered successfully!  You may now login with your new credentials");
+                window.location = "index.php";
+            }else{
+                alert("Problems registering you as a new user.  Please try again");
+            }
+        }else{
+            document.getElementById("registerButtonId").innerHTML = "Loading...";
+        }
+    };
+    httpReq.open("GET", "functions.php?type=Register&username=" + username +
+        "&password=" + password + "&firstname=" + firstname + "&lastname=" + lastname + "&email=" + email);
+    httpReq.send(null);
+}
+
+function loadCategories() {
+    let httpReq = new XMLHttpRequest();
+    httpReq.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+
+            document.getElementById("categoriesTable").innerHTML = this.responseText;
+
+        }
+    };
+    httpReq.open("GET", "functions.php?type=LoadCategories");
+    httpReq.send(null);
+}
+function loadTopics() {
+    let httpReq = new XMLHttpRequest();
+    httpReq.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+
+            document.getElementById("topicsTable").innerHTML = this.responseText;
+        }
+
+    };
+    httpReq.open("GET", "functions.php?type=LoadTopics");
+    httpReq.send(null);
+}
+function loadPosts() {
+    let httpReq = new XMLHttpRequest();
+    httpReq.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+
+            document.getElementById("postsTable").innerHTML = this.responseText;
+        }
+
+    };
+    httpReq.open("GET", "functions.php?type=LoadPosts");
+    httpReq.send(null);
+}
+function checkCategoryFields(){
+
+    let catName = document.getElementById('category_name').value;
+    let catDesc = document.getElementById('category_desc').value;
+
+    if (catName !== "" && catDesc !== ""){
+        console.log(catName + catDesc);
+        createCategory(catName, catDesc);
+    }else{
+        alert("Please fill in all required fields");
+    }
+}
+function createCategory(catName, catDesc) {
+    let httpReq = new XMLHttpRequest();
+    httpReq.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+
+            if(this.responseText == true){
+                alert("New category created successfully!");
+                window.location = "forum.php";
+            }else{
+                alert("Problems creating new category.  Please try again");
+            }
+        }
+    };
+    httpReq.open("GET", "functions.php?type=CreateCategory&catName=" + catName + "&catDesc=" + catDesc);
+    httpReq.send(null);
+}
+function checkTopicFields(){
+
+    let topicName = document.getElementById('topic_name').value;
+    let topicDesc = document.getElementById('topic_desc').value;
+
+    if (topicName !== "" && topicDesc !== ""){
+        createTopic(topicName, topicDesc);
+    }else{
+        alert("Please fill in all required fields");
+    }
+}
+function createTopic(topicName, topicDesc) {
+    let httpReq = new XMLHttpRequest();
+    httpReq.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+
+            if(this.responseText == true){
+                alert("New topic created successfully!");
+                location.reload();
+            }else{
+                alert("Problems creating new topic.  Please try again");
+            }
+        }
+    };
+    httpReq.open("GET", "functions.php?type=CreateTopic&topicName=" + topicName + "&topicDesc=" + topicDesc);
+    httpReq.send(null);
+}
+
+function checkPostFields(){
+    let postText = document.getElementById('post_desc').value;
+
+    if (postText !== ""){
+        createPost(postText);
+    }else{
+        alert("Please fill in all required fields");
+    }
+}
+function createPost(postText) {
+    let httpReq = new XMLHttpRequest();
+    httpReq.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+
+            if(this.responseText == true){
+                alert("New post created successfully!");
+                location.reload();
+            }else{
+                alert("Problems creating new category.  Please try again");
+            }
+
+        }
+    };
+    httpReq.open("GET", "functions.php?type=CreatePost&postText=" + postText );
+    httpReq.send(null);
+}
+function showDiv(divId, element) {
+    document.getElementById(divId).style.display = element.value !== "" ? 'block' : 'none';
+}
+
+function checkSearchFields(){
+    let searchText_upper = document.getElementById('pokemon_search').value;
+    let dropdown = document.getElementById("search_type");
+    let searchType = dropdown.options[dropdown.selectedIndex].value;
+
+    let searchText = searchText_upper.toLowerCase();
+
+    if (searchText !== ""){
+        createSearch(searchText, searchType);
+
+    }else{
+        alert("Please fill in all required fields");
+    }
+}
+
+function createSearch(searchText, searchType) {
+
+    let httpReq = new XMLHttpRequest();
+    httpReq.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+
+            document.getElementById("search_results").innerHTML = this.responseText;
+
+        }
+    };
+    httpReq.open("GET", "functions.php?type=Search&searchType=" + searchType + "&searchText=" + searchText);
+    httpReq.send(null);
+}
+
+function singlePokeSearch() {
+
+    let httpReq = new XMLHttpRequest();
+    httpReq.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+
+            document.getElementById("pokemon_results").innerHTML = this.responseText;
+
+        }
+    };
+    httpReq.open("GET", "functions.php?type=SinglePokeSearch");
+    httpReq.send(null);
+}
+
+function addPokemon() {
+
+    let httpReq = new XMLHttpRequest();
+    httpReq.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+
+            if(this.responseText == true){
+                alert("Pokemon successfully added to your team!");
+                window.location = "profile.php";
+            }else{
+                alert("Problem adding pokemon to your team.  Please try again.  Note: you may only have 6 pokeomon on your team ata time");
+            }
+        }
+    };
+    httpReq.open("GET", "functions.php?type=AddPokemon");
+    httpReq.send(null);
+}
+
+function loadPokemon() {
+
+    let httpReq = new XMLHttpRequest();
+    httpReq.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+
+            console.log(this.responseText);
+            document.getElementById("users_pokemon").innerHTML = this.responseText;
+
+        }
+    };
+    httpReq.open("GET", "functions.php?type=LoadPokemon");
+    httpReq.send(null);
+}
+
+function leaderboard() {
+
+    let httpReq = new XMLHttpRequest();
+    httpReq.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+
+            console.log(this.responseText);
+            document.getElementById("leaderboard_results").innerHTML = this.responseText;
+
+        }
+    };
+    httpReq.open("GET", "functions.php?type=LoadLeaderboard");
+    httpReq.send(null);
+}
+
+function battle() {
+
+    let username_toBattle = document.getElementById('username_toBattle').value;
+
+    let httpReq = new XMLHttpRequest();
+    httpReq.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+
+            console.log(this.responseText);
+
+            if(this.responseText == 1){
+                alert("Congratulations, your team won!");
+                window.location = "leaderboard.php";
+            }else{
+                alert("Sorry, you lose :(  Better luck next time!");
+                window.location = "leaderboard.php";
+            }
+
+        }
+    };
+    httpReq.open("GET", "functions.php?type=Battle&useranme_toBattle=" + username_toBattle);
+    httpReq.send(null);
+}
+
+
+
+
+
